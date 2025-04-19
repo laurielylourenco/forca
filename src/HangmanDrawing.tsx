363435a71920731fe22456e7c9254c632b2fa030
Lauriely Lourenço <laurielylourenco@gmail.com>
2025-04-19 16:52:07 -0300

    Css da forca e boneco

--- src/HangmanDrawing.tsx
+++ src/HangmanDrawing.tsx
@@ -1,115 +1,34 @@
+import "./css/HangmanDrawing.css"
 
-const HEAD = (
-    <div style={{
-        width: '50px',
-        height: '50px',
-        borderRadius: '100%',
-        border: '10px solid black',
-        position: 'absolute',
-        top: '50px',
-        right: '-30px'
-    }}>
-
-    </div>
-)
-
-
-const BODY = (
-    <div style={{
-        width: '10px',
-        height: '100px',
-        background: 'black',
-        position: 'absolute',
-        top: '120px',
-        right: 0
-    }}>
-
-    </div>
-)
-
-const RIGHT_ARM = (
-    <div style={{
-        width: '100px',
-        height: '10px',
-        background: 'black',
-        position: 'absolute',
-        top: '150px',
-        right: "-100px",
-        rotate: "-30deg",
-        transformOrigin: 'left bottom'
-    }}>
-
-    </div>
-)
-
-
-const LEFT_ARM = (
-    <div style={{
-        width: '100px',
-        height: '10px',
-        background: 'black',
-        position: 'absolute',
-        top: '150px',
-        right: "10px",
-        rotate: "30deg",
-        transformOrigin: 'right bottom'
-    }}>
-
-    </div>
-)
-
-
-const RIGHT_LEG = (
-    <div style={{
-        width: '100px',
-        height: '10px',
-        background: 'black',
-        position: 'absolute',
-        top: '210px',
-        right: "-90px",
-        rotate: "60deg",
-        transformOrigin: 'left bottom'
-    }}>
-
-    </div>
-)
-
-
-const LEFT_LEG = (
-    <div style={{
-        width: '100px',
-        height: '10px',
-        background: 'black',
-        position: 'absolute',
-        top: '210px',
-        right: 0,
-        rotate: "-60deg",
-        transformOrigin: 'right bottom'
-    }}>
-
-    </div>
-)
-type HangmanDrawingrops = {
-    numberOfGuesses: number
+type HangmanDrawingProps = {
+  numberOfGuesses: number
 }
 
-const BODY_PARTS = [HEAD, BODY, RIGHT_ARM, LEFT_ARM, RIGHT_LEG, LEFT_LEG];
+const HEAD = <div className="hangman-part head"></div>
 
+const BODY = <div className="hangman-part body"></div>
 
+const RIGHT_ARM = <div className="hangman-part right-arm"></div>
 
-export function HangmanDrawing({ numberOfGuesses }: HangmanDrawingrops) {
-    return <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-    <div style={{ position: 'relative' }}>
-      {BODY_PARTS.slice(0, numberOfGuesses)}
-  
-      <div style={{ height: '50px', width: '10px', background: 'black', position: 'absolute', top: 0, right: 0 }}></div>
-  
-      <div style={{ height: '10px', width: '200px', background: 'black', marginLeft: '120px' }}></div>
-  
-      <div style={{ height: '300px', width: '10px', background: 'black', marginLeft: '120px' }}></div>
-  
-      <div style={{ height: '10px', width: '250px', background: 'black', margin: 'auto' }}></div>
+const LEFT_ARM = <div className="hangman-part left-arm"></div>
+
+const RIGHT_LEG = <div className="hangman-part right-leg"></div>
+
+const LEFT_LEG = <div className="hangman-part left-leg"></div>
+
+const BODY_PARTS = [HEAD, BODY, RIGHT_ARM, LEFT_ARM, RIGHT_LEG, LEFT_LEG]
+
+export function HangmanDrawing({ numberOfGuesses }: HangmanDrawingProps) {
+  return (
+    <div className="hangman-container">
+      <div className="hangman-drawing">
+        {BODY_PARTS.slice(0, numberOfGuesses)}
+
+        <div className="hangman-rope"></div>
+        <div className="hangman-beam"></div>
+        <div className="hangman-post"></div>
+        <div className="hangman-base"></div>
+      </div>
     </div>
-  </div>
-  
+  )
 }
